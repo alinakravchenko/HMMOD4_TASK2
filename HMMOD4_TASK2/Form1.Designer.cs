a995e0025ba974affd4d356f8e750636fb1eea31
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2023-02-25 12:24:13 +0300

    Add project files.

---
+++ HMMOD4_TASK2/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace HMMOD4_TASK2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.listBoxNumber = new System.Windows.Forms.ListBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonStart.Location = new System.Drawing.Point(314, 188);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(110, 57);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // listBoxNumber
+            // 
+            this.listBoxNumber.FormattingEnabled = true;
+            this.listBoxNumber.ItemHeight = 15;
+            this.listBoxNumber.Location = new System.Drawing.Point(14, 12);
+            this.listBoxNumber.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.listBoxNumber.Name = "listBoxNumber";
+            this.listBoxNumber.ScrollAlwaysVisible = true;
+            this.listBoxNumber.Size = new System.Drawing.Size(233, 394);
+            this.listBoxNumber.TabIndex = 1;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelInfo.Location = new System.Drawing.Point(14, 436);
+            this.labelInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(162, 19);
+            this.labelInfo.TabIndex = 2;
+            this.labelInfo.Text = "Информация о потоках";
+            this.labelInfo.Click += new System.EventHandler(this.labelInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 473);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.listBoxNumber);
+            this.Controls.Add(this.buttonStart);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Мьтекс";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListBox listBoxNumber;
+        private System.Windows.Forms.Label labelInfo;
+    }
+}
---
+++ HMMOD4_TASK2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows.Forms;
+
+namespace HMMOD4_TASK2
+{
+    public partial class Form1 : Form
+    {
+        Mutex mutex = new Mutex();
+        public Form1()
+        {
+            InitializeComponent();
+            buttonStart.Click += ButtonStart_Click;
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            Thread thread1 = new Thread(Thread1Numbers);
+            thread1.Name = "Поток 1";
+            Thread thread2 = new Thread(Thread2Numbers);
+            thread2.Name = "Поток 2";
+            thread1.Start();
+            thread2.Start();
+        }
+
+        private void Thread1Numbers()
+        {
+            mutex.WaitOne();
+            for (int i = 0; i <= 20; i++)
+            {
+                Thread.Sleep(1000);
+                Invoke(new Action<string>((threanName) =>
+                {
+                    labelInfo.Text = threanName + " начал свою работy";
+                    listBoxNumber.Items.Add(i);
+                }), Thread.CurrentThread.Name);
+            }
+            mutex.ReleaseMutex();
+        }
+
+        private void Thread2Numbers()
+        {
+            mutex.WaitOne();
+            for (int i = 10; i >= 0; i--)
+            {
+                Thread.Sleep(1000);
+                Invoke(new Action<string>((threanName) =>
+                {
+                    labelInfo.Text = "\n" + threanName + " начал свою работу";
+                    listBoxNumber.Items.Add(i);
+                }), Thread.CurrentThread.Name);
+            }
+            mutex.ReleaseMutex();
+        }
+
+        private void labelInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
